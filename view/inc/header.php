762bce12ea9394d61bd1a65c637b9d2d4d79d3a2
leerlandais <leerlandais@gmail.com>
2024-06-13 07:25:53 +0100

    added welcome

--- view/inc/header.php
+++ view/inc/header.php
@@ -1,35 +1,41 @@
 <header class="m-5" id="headerNormal">
-<p class="h1 text-center" id="homepage_h1"></p>
+    <p class="h1 text-center" id="homepage_h1"></p>
 
-<nav class="navbar">
-<div class="container-fluid">
-
-    <div class="collapse-sm navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-row justify-content-evenly">
-      <li class="nav-item">
-        <a class="nav-link" href="?" id="navHome"></a>
-        </li>
+<nav class="navbar border border-2 border-secondary">
+    <div class="container-fluid">
+        
+        <div class="collapse-sm navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-column justify-content-evenly align-items-end">
+                <?php if(!isset($_SESSION["id"])) {
+                    ?>
         <li class="nav-item">
-        <a class="nav-link" aria-current="page" href="./" id="navContact"></a>
+            <a class="nav-link" aria-current="page" href="?login" id="navLogin"></a>
         </li>
-        <li class="nav-item">
-        <a class="nav-link" aria-current="page" href="?about" id="navAbout"></a>
-        </li>
-
-    <?php if(!isset($_SESSION["id"])) {
-      ?>
-        <li class="nav-item">
-        <a class="nav-link" aria-current="page" href="?login" id="navLogin"></a>
-        </li>
-        <?php
+    <?php
         }
         ?>
-
-<form method="POST" class="d-flex flex-row">
-    <button class="btn langButton langEN" id="englishButton" value="en" type="submit" name="user_lang"></button>
-    <button class="btn langButton langFR" id="frenchButton" value="fr" type="submit" name="user_lang"></button>
+      </ul>
+    
+    <form method="POST" class="d-flex flex-row">
+        <button class="btn langButton langEN" id="englishButton" value="en" type="submit" name="user_lang"></button>
+        <button class="btn langButton langFR" id="frenchButton" value="fr" type="submit" name="user_lang"></button>
 </form>
+
 </div>
-  </div>
+</div>
 </nav>
+<div class="container-fluid d-flex flex-row justify-content-center">
+<p class="h2" id="welcomeVisitor"></p>
+<?php
+    if (isset($visitorName)) {
+?>
+<p class="h2" id="welcomeName"></p>
+<?php
+    }else {
+?>
+<p class="h2"> , Visitor</p>
+<?php
+}
+?>
+</div>
 </header>
