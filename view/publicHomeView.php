9c927649a4a8e88a618e45c951b8f9697684d7cf
leerlandais <leerlandais@gmail.com>
2024-06-16 14:14:06 +0100

    added form

--- view/publicHomeView.php
+++ view/publicHomeView.php
@@ -15,18 +15,26 @@
 <?php include ("inc/header.php"); ?>
 <?php include ("inc/error-message.php"); // leave this here to display any eventual error message - include this on all pages ?>
     
-    <div class="container d-flex flex-column align-items-center">
-  
-    <?php
+    <div class="container">
+        <?php if (isset($_GET["login"])) include("inc/loginForm.php"); ?>
 
-        if (isset($_GET["login"])) include("inc/loginForm.php");
-    ?>
+            <form action="" method="POST" class="d-flex flex-column align-items-center">
+                
+                    <label for="contactName" id="contactNameLabel"></label>
+                        <input type="text" class="text-center" name="contactName" id="contactName" placeholder="<?php if (isset($visitorName)) echo $visitorName["cp_visitor_name"]?>" value="<?php if (isset($visitorName)) echo $visitorName["cp_visitor_name"]?>" required>
+                    <label for="contactMail" id="contactMailLabel"></label>
+                        <input type="email" class="text-center w-25" name="contactMail" id="contactMail" placeholder="<?php if (isset($visitorName)) echo $visitorName["cp_visitor_email"]?>" value="<?php if (isset($visitorName)) echo $visitorName["cp_visitor_email"]?>" required>
+                    <label for="contactMess" id="contactMessLabel"></label>
+                        <textarea name="contactMess" id="contactMess" class="w-50" required></textarea>
+                
+                <button class="btn btn-light btn-outline-success submitButton mt-3"></button>
+            </form>
+<?php // var_dump($visitorName); ?>
+        
+        
+        
     </div>
-
-    <?php
-        // INCLURE SI NECESSAIRE
-        include("inc/footer.php");
-    ?>
+    <?php include("inc/footer.php"); ?>
     </div>
     <!-- BOOTSTRAP CDN[SCRIPTS] ICI -->    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
